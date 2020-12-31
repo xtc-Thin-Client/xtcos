133642b9c016c4563fffd7b0f593ad2d208dc84b
Stephan Eisler <stephan@eisler.de>
2020-12-31 15:40:16 +0100

    add bintray repository PublicKey

--- stagetm/01-configure/00-run.sh
+++ stagetm/01-configure/00-run.sh
@@ -2,6 +2,7 @@
 
 on_chroot <<EOF
 wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=xtc-thin-client | apt-key add -
+wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | apt-key add -
 echo "deb https://dl.bintray.com/xtc-thin-client/xtc buster main" > /etc/apt/sources.list.d/xtc.list
 apt-get update
 apt-get -y install xtcbase xtc tightvncserver
