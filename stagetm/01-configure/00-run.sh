2d474d29fcf5f244892940212f64e0c1d22ef7a5
Stephan Eisler <stephan@eisler.de>
2020-12-31 10:09:19 +0100

    Update 00-run.sh
    add xtc public key

--- stagetm/01-configure/00-run.sh
+++ stagetm/01-configure/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 on_chroot <<EOF
+wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=xtc-thin-client | apt-key add -
 echo "deb https://dl.bintray.com/xtc-thin-client/xtc buster unstable" > /etc/apt/sources.list.d/xtc.list
 apt-get update
 apt-get -y install xtcbase xtc
