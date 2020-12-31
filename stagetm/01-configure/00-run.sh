d47e9cc325fd16f1587c7d866256223ef31c036f
Stephan Eisler <stephan@eisler.de>
2020-12-31 15:22:55 +0100

    Update 00-run.sh

--- stagetm/01-configure/00-run.sh
+++ stagetm/01-configure/00-run.sh
@@ -2,7 +2,7 @@
 
 on_chroot <<EOF
 wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=xtc-thin-client | apt-key add -
-echo "deb https://dl.bintray.com/xtc-thin-client/xtc buster unstable" > /etc/apt/sources.list.d/xtc.list
+echo "deb https://dl.bintray.com/xtc-thin-client/xtc buster main" > /etc/apt/sources.list.d/xtc.list
 apt-get update
-apt-get -y install xtcbase xtc
+apt-get -y install xtcbase xtc tightvncserver
 EOF
