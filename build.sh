a649517efcc7f3703c733cfe2be02e53277be3a5
Stephan Eisler <stephan@eisler.de>
2021-01-02 12:07:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ cp -r ./stagetm ./pi-gen
 cd pi-gen
 echo "IMG_NAME='xtcos'" > config
 echo "HOSTNAME='xtc'" >> config
-echo "FIRST_USER_NAME='pi'" >> config
-echo "FIRST_USER_PASS='...'" >> config
+echo "FIRST_USER_NAME='thinclient'" >> config
+echo "FIRST_USER_PASS='thinclient'" >> config
 echo "ENABLE_SSH='1'" >> config
 echo "TIMEZONE_DEFAULT='Europe/Berlin'" >> config
 echo "STAGE_LIST='stage0 stage1 stage2 stagetm'" >> config
