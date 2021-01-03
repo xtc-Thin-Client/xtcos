bdbb54b5025222114d8c6e4ab46f837ff55e94b1
Stephan Eisler <stephan@eisler.de>
2021-01-03 09:01:22 +0100

    add stage3

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "FIRST_USER_NAME='thinclient'" >> config
 echo "FIRST_USER_PASS='thinclient'" >> config
 echo "ENABLE_SSH='1'" >> config
 echo "TIMEZONE_DEFAULT='Europe/Berlin'" >> config
-echo "STAGE_LIST='stage0 stage1 stage2 stagetm'" >> config
+echo "STAGE_LIST='stage0 stage1 stage2 stage3 stagetm'" >> config
 touch ./stage4/SKIP ./stage5/SKIP
 touch ./stage2/SKIP_IMAGES ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 sudo ./build-docker.sh
