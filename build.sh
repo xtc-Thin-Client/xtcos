a0cf889d6bdbf709a7bf6c73773aee4da0376986
Stephan Eisler <stephan@eisler.de>
2021-01-06 20:26:30 +0100

    add /data partition patch

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/RPi-Distro/pi-gen
-cp -r ./stagetm ./pi-gen
+cp -r ./stagextc ./pi-gen
 cp -r ./patch ./pi-gen
 cd pi-gen
 echo "IMG_NAME='xtcos'" > config
@@ -10,7 +10,7 @@ echo "FIRST_USER_NAME='thinclient'" >> config
 echo "FIRST_USER_PASS='thinclient'" >> config
 echo "ENABLE_SSH='1'" >> config
 echo "TIMEZONE_DEFAULT='Europe/Berlin'" >> config
-echo "STAGE_LIST='stage0 stage1 stage2 stage3 stagetm'" >> config
+echo "STAGE_LIST='stage0 stage1 stage2 stage3 stagextc'" >> config
 touch ./stage4/SKIP ./stage5/SKIP
 touch ./stage2/SKIP_IMAGES ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 sudo ./build-docker.sh
