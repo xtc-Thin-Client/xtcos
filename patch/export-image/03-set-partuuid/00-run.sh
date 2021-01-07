bdc6ee4f1d9ac0d5865b64c758c744921faabcc4
Stephan Eisler <stephan@eisler.de>
2021-01-07 08:17:22 +0100

    Update 00-run.sh

--- patch/export-image/03-set-partuuid/00-run.sh
+++ patch/export-image/03-set-partuuid/00-run.sh
@@ -5,11 +5,11 @@ IMG_FILE="${STAGE_WORK_DIR}/${IMG_FILENAME}${IMG_SUFFIX}.img"
 IMGID="$(dd if="${IMG_FILE}" skip=440 bs=1 count=4 2>/dev/null | xxd -e | cut -f 2 -d' ')"
 
 BOOT_PARTUUID="${IMGID}-01"
-ROOT_PARTUUID="${IMGID}-02"
-DATA_PARTUUID="${IMGID}-03"
+DATA_PARTUUID="${IMGID}-02"
+ROOT_PARTUUID="${IMGID}-03"
 
 sed -i "s/BOOTDEV/PARTUUID=${BOOT_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
-sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
 sed -i "s/DATADEV/PARTUUID=${DATA_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
+sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
 
 sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID}/" "${ROOTFS_DIR}/boot/cmdline.txt"
