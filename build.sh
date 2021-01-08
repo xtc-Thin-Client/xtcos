7bbf783a81162e4caaa5d44f6b06975b4e717445
Stephan Eisler <stephan@eisler.de>
2021-01-08 13:26:04 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "FIRST_USER_NAME='thinclient'" >> config
 echo "FIRST_USER_PASS='thinclient'" >> config
 echo "ENABLE_SSH='1'" >> config
 echo "TIMEZONE_DEFAULT='Europe/Berlin'" >> config
-echo "STAGE_LIST='stage0 stage1 stage2 stage3 stagextc'" >> config
+echo "STAGE_LIST='stage0 stage1 stage2 stagextc'" >> config
 touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
 touch ./stage2/SKIP_IMAGES ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 sudo ./build-docker.sh
