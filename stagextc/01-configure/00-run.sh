4abed5b29d646151d2c8043c889d210671b263fc
Stephan Eisler <stephan@eisler.de>
2021-02-13 13:15:03 +0100

    fix "cannot create /dev/null: Permission denied"

--- stagextc/01-configure/00-run.sh
+++ stagextc/01-configure/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 on_chroot <<EOF
+chmod go+rw /dev/null
 wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=xtc-thin-client | apt-key add -
 wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | apt-key add -
 echo "deb https://dl.bintray.com/xtc-thin-client/xtc buster main" > /etc/apt/sources.list.d/xtc.list
