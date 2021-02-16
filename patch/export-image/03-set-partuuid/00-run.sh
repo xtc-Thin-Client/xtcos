54057a5a5c1be681186360346ce10b4774ee71c7
Stephan Eisler <stephan@eisler.de>
2021-02-16 12:15:01 +0100

    Update 00-run.sh

--- patch/export-image/03-set-partuuid/00-run.sh
+++ patch/export-image/03-set-partuuid/00-run.sh
@@ -10,7 +10,7 @@ if [ "${NO_PRERUN_QCOW2}" = "0" ]; then
 	ROOT_PARTUUID="${IMGID}-02"
 
 	sed -i "s/BOOTDEV/PARTUUID=${BOOT_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
-    sed -i "s/DATADEV/PARTUUID=${DATA_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
+    	sed -i "s/DATADEV/PARTUUID=${DATA_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
 	sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID}/" "${ROOTFS_DIR}/etc/fstab"
 
 	sed -i "s/ROOTDEV/PARTUUID=${ROOT_PARTUUID}/" "${ROOTFS_DIR}/boot/cmdline.txt"
